1ff5ea123c615aadbf6985428b3cf1eacc0251d7
annekellen <anneaccarini@gmail.com>
2021-03-22 22:48:08 -0300

    criando formularios de avaliacao e cadastro

---
+++ src/components/Formularios/Form/Avaliacao/index.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import "./style.css";
+import FormSignup from "../Avaliacao/FormSignUp"
+import FormSuccess from "../Avaliacao/FormSucces";
+
+const Form = () => {
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  function submitForm() {
+    setIsSubmitted(true);
+  }
+  return (
+    <>
+      <div className="form-container">
+        {!isSubmitted ? (
+          <FormSignup submitForm={submitForm} />
+        ) : (
+          <FormSuccess />
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Form;
