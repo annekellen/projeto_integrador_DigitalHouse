456e803eb5129c3505e653350a3061d680319d56
eduardo rezende cruz <edu_rezende11@hotmail.com>
2021-05-13 15:14:46 -0300

    simulando api para prover imagens e descrição do produto

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,11 +1,14 @@
 import BotaoPrincipal from '../Componentes-Gerais/Botoes/BotaoPrincipal'
 import './style.css'
-const Card = () => {
+const Card = ({produto}) => {
+  
   return (
     <article className="card">
-      <div className="card__foto"></div>
-      <h3 className="card__title">Lorem ipsum is a pseudo-Latin</h3>
-      <p className="card__preco">R$29,99</p>
+      <div className="card__foto">
+        <img className="card__foto" src={produto.photo} alt={produto.name} />
+      </div>
+      <h3 className="card__title">{produto.name}</h3>
+      <p className="card__preco">{`R$ ${produto.price}`}</p>
       <BotaoPrincipal texto="Adicionar Card" />
 
 
--- src/components/Prateleiras/index.js
+++ src/components/Prateleiras/index.js
@@ -1,13 +1,46 @@
 import Card from '../Card'
 import './style.css'
+
+const produtos = [
+  {
+    "name": "t-shirt",
+    "price": 48,
+    "photo": "https://i.ibb.co/51Szwz0/alex-haigh-f-Et6-Wd4t4j0-unsplash.jpg"
+  },
+  {
+    "name": "Amora",
+    
+    "photo": "https://www.imagemhost.com.br/images/2021/05/11/faith-yarn-Wr0TpKqf26s-unsplash.jpg",
+    "price": 45
+  },
+  {
+    "name": "camiseta",
+    "photo": "https://www.imagemhost.com.br/images/2021/05/11/mulher.png",
+    "price": 20
+  },
+  {
+    "name": "verde",
+    "photo": "https://www.imagemhost.com.br/images/2021/05/11/homem.png",
+    "price": 10
+  }
+
+]
 const Prateleiras = () => {
   return (
     <section className="prateleiras">
     
+
+    {
+      produtos.map(
+        (produto)=><Card produto={produto}/>
+        
+      )
+    }
+    
+     {/*  <Card />
       <Card />
       <Card />
-      <Card />
-      <Card />
+      <Card /> */}
     </section>
   )
 }
